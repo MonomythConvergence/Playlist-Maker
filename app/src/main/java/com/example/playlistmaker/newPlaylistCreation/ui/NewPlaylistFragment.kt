39aa1152243e25e33d3eadd25b37ce5944e9e89e
Monomythconvergence <shoulddoisuppose@gmail.com>
2024-09-18 12:55:32 +0300

    Sprint22 try3 + Uri and Constants fix + quickfix

--- app/src/main/java/com/example/playlistmaker/newPlaylistCreation/ui/NewPlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/newPlaylistCreation/ui/NewPlaylistFragment.kt
@@ -20,7 +20,6 @@ import android.graphics.BitmapFactory
 import android.os.Build
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getDrawable
 import com.example.playlistmaker.Constants
